2e491621904afb1c9a24a271257e510714b6db3f
ngominhtuan <tuanktxmhv@gmail.com>
2023-08-08 09:53:31 +0700

    FristWebApi

---
+++ NewApp/giai phuong trinh.cs
@@ -0,0 +1,10 @@
+//khai bao bien so
+double heSoA, heSoB, nghiemX;
+//yeu cau nguoi dung nhap so lieu
+System.Console.WriteLine("a = ");
+//chuyen doi kieu du lieu tu string => float
+heSoA = Convert.ToDouble(console.ReadLine());
+//yeu cau nguoi dung nhap so lieu
+System.Console.WriteLine("b = ");
+//giai phuong trinh
+
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.Write(6);
+//int a = 9;
+//Console.WriteLine("a = " + a);
+int a = 9;
+int b = 3;
+Console.WriteLine("{0} + {1} = {2}", a, b, a+b);
